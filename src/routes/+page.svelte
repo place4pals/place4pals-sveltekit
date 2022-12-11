42315a207298dd1d260fe626d77fb480a811c848
Chris Aitken <chris@heythisischris.com>
2022-12-11 14:24:21 -0500

    Created base SvelteKit project with adapter-static

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>place4pals</title>
+	<meta name="description" content="non-profit social media" />
+</svelte:head>
+
+<section>
+	place4pals clone built using sveltekit
+</section>
+
+<style>
+</style>
